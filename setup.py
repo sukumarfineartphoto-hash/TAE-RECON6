bda3cf0ef66d4e8f4af445eb653eb0312ae9bbe0
N. Sukumar <sukumarfineartphoto@gmail.com>
2026-06-18 21:49:34 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+"""Thin setup.py shim; actual metadata lives in pyproject.toml.
+
+Included for compatibility with tooling that expects setup.py to exist.
+"""
+from setuptools import setup
+
+setup()
