1415e1fd7f7e1a6854e0fc84cb646fac032af04f
N. Sukumar <sukumarfineartphoto@gmail.com>
2026-06-18 21:44:19 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-"""Thin setup.py shim; actual metadata lives in pyproject.toml.
-
-Included for compatibility with tooling that expects setup.py to exist.
-"""
-from setuptools import setup
-
-setup()
